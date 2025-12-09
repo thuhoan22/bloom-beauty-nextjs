e2ea4785abee56e9db4f1e32143d7bd434923d8e
NguyenThuHoan_환 <hoanntt@hivelab.com>
2025-12-09 10:22:20 +0700

    update about us and search

---
+++ app/about/AboutUs.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+import "./AboutUs.scss";
+
+export default function AboutUs() {
+  return (
+    <main className="main-content about-page">
+      <div className="inner">
+
+      </div>
+    </main>
+  )
+}
